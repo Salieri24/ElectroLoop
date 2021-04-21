f35ed7963b20c9c629e0779c0067649876c273d9
Angel <Angel.G.Prodanov@gmail.com>
2021-04-21 22:48:57 +0300

    Movement Rework 01 + Sample animation + Camera rework 01

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,6 +5,9 @@ public class FollowCamera : MonoBehaviour
     public Vector3 offset;
     public float smooth;
     public Transform target;
+    public Rigidbody2D targetRigid;
+
+    public float maxCamSize;
 
     private Camera cam;
 
@@ -18,7 +21,8 @@ public class FollowCamera : MonoBehaviour
         Vector3 smoothPosition = Vector3.Lerp(desiredPosition, transform.position, smooth);
         this.transform.position = smoothPosition;
 
-        cam.orthographicSize = Mathf.Abs(desiredPosition.x - smoothPosition.x)*10+3;
-
+        cam.orthographicSize = Mathf.Abs(targetRigid.velocity.x)*0.2f+3;
+        if (cam.orthographicSize >= maxCamSize)
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize,maxCamSize, smooth);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,27 +1,62 @@
 using UnityEngine;
-using Live2D.Cubism.Core;
+//using Live2D.Cubism.Core;
+using UnityEngine.UI;
+
 public class Movement : MonoBehaviour
 {
-    private CubismModel model;
+    [SerializeField] private LayerMask platformLayerMask;
+    //private CubismModel model;
     private Rigidbody2D rigid;
-    public float thrust = 1;
+    private CircleCollider2D circleColl;
+
+    public Text targetText;
+    public float thrust = 3;
+    public float maxSpeed;
+
     // Start is called before the first frame update
     public void Start()
     {
-        model = GetComponent<CubismModel>();
+        Debug.developerConsoleVisible = true;
+        //model = GetComponent<CubismModel>();
         rigid = GetComponent<Rigidbody2D>();
+        circleColl = GetComponent<CircleCollider2D>();
+
     }
 
     // Update is called once per frame
-    public void Update()
+    public void FixedUpdate()
     {
 
         float f = Input.GetAxis("Horizontal");
-        Vector2 v = new Vector2(f*thrust ,0);
+        Vector2 v = new Vector2(0,0);
+
+        bool grounded = isGrounded();
+
+        if(grounded && Input.GetKey(KeyCode.Space)) //jump
+        {
+            rigid.AddForce(new Vector2(0, 5f), ForceMode2D.Impulse);
+        }
+
+        if (!grounded)
+        {
+            v.x = (f * thrust) * 0.25f;
+        }
+        else
+        {
+            v.x = f * thrust;
+        }
         rigid.AddForce(v);
 
-        model.Parameters[3].Value = Mathf.Abs(rigid.velocity.x)*2.5f;
-        model.Parameters[0].Value = rigid.velocity.x * -1;
-        model.Parameters[1].Value = rigid.velocity.y * -1;
+        if (Mathf.Abs(rigid.velocity.x) >= maxSpeed && rigid.velocity.x * f > 0)   //max speed
+            rigid.AddForce(new Vector2(-v.x, 0));
+
+        targetText.text = "Thrust = " + thrust + "\nmaxSpeed = " + maxSpeed + "\nVelocity = "+rigid.velocity+"\n";
+        targetText.text += "Applied force:" + v + "\nGrounded: " + grounded;
+    }
+    private bool isGrounded()
+    {
+
+        var grounded = Physics2D.OverlapCircle(circleColl.bounds.center,circleColl.bounds.size.x/2+0.01f,platformLayerMask);
+        return grounded != null;
     }
 }
